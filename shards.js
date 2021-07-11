c980c9f7b1fb9234b6a538b41feafeff177fafa6
Nek <68248147+NekWasTaken@users.noreply.github.com>
2021-07-11 00:29:58 -0700

    Update shards.js

--- shards.js
+++ shards.js
@@ -5,9 +5,6 @@ const Statcord = require('statcord.js')
 const manager = new ShardingManager('./ToD.js', { token: process.env.Token, autoSpawn: true, respawn: true});
 const chalk = require('chalk')
 
-// Spawn shards, statcord works with both auto and a set amount of shards
-
-// Normal shardCreate event
 manager.on("shardCreate", (shard) => {
     console.log(console.log(chalk.greenBright(`✅ - Launched shard #${shard.id}\n───────────────────────────────`)));
 });
