5875480f99f775fe7fc0a42a581e0be951af43f9
Nek <68248147+NekWasTaken@users.noreply.github.com>
2021-07-11 08:51:53 -0700

    Attempt 3 to fix the sharding issue.

--- shards.js
+++ shards.js
@@ -8,4 +8,4 @@ const chalk = require('chalk')
 manager.on("shardCreate", (shard) => {
     console.log(console.log(chalk.greenBright(`✅ - Launched shard #${shard.id}\n───────────────────────────────`)));
 });
-manager.spawn(undefined, undefined, 1200)
+manager.spawn(”auto”, undefined, 1200)
