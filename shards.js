a245a409e472e26ec80ffff6c450bcdc3afe9e59
Nek <68248147+NekWasTaken@users.noreply.github.com>
2021-07-11 09:59:23 -0700

    Attempt 7 at trying to fix the sharding issue.

--- shards.js
+++ shards.js
@@ -2,10 +2,10 @@ const { ShardingManager } = require('discord.js');
 const logger = require('./modules/logger')
 require('dotenv').config()
 const Statcord = require('statcord.js')
-const manager = new ShardingManager('./ToD.js', { token: process.env.Token, totalShards: `auto`, respawn: true});
+const manager = new ShardingManager('./ToD.js', { token: process.env.Token, totalShards: `3`, respawn: true});
 const chalk = require('chalk')
 
 manager.on("shardCreate", (shard) => {
     console.log(console.log(chalk.greenBright(`✅ - Launched shard #${shard.id}\n───────────────────────────────`)));
 });
-manager.spawn(`auto`, undefined, 1200)
+manager.spawn(undefined, undefined, -1)
