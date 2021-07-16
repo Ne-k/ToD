e1d1e7ae7a19fa5b3c7ff0d6a33a52d1a17ae34e
Nek <username.lilrice@gmail.com>
2021-07-15 17:54:25 -0700

    Re-enabled the suggest command.

--- commands/Misc/suggest.js
+++ commands/Misc/suggest.js
@@ -7,9 +7,9 @@ module.exports = {
       description: "Send a bot suggestion to the development server to be reviewed and may be added. It can be new `truth` or `dare` responses or new command ideas.",
     
     },
-    run: async (bot, message, args) => {
+    run: async (client, message, args) => {
       if (talkedRecently.has(message.author.id)) {
-        return message.channel.send(new MessageEmbed().setDescription("As much as we appreciate your suggestion, please wait up to **1 minute** before using this command again, this is to prevent spam.").setColor('RANDOM'));
+        return message.channel.send({embeds: [new MessageEmbed().setDescription("As much as we appreciate your suggestion, please wait up to **1 minute** before using this command again, this is to prevent spam.").setColor('RANDOM')]});
 } else {
   let reportedbug = args.join(" ").slice(0);
   let messageuser = message.author.username;
@@ -22,13 +22,13 @@ if(reportedbug.toLowerCase() === 'dare') {
 let nodareembed = new MessageEmbed()
 .setColor('RED')
 .setDescription('You are unable to suggest "`dare`", please suggest something else.')
-return message.channel.send(nodareembed)
+return message.channel.send({embeds: [nodareembed]})
 }
 if(reportedbug.toLowerCase() === 'truth') {
 let nodareembed = new MessageEmbed()
 .setColor('RED')
 .setDescription('You are unable to suggest "`truth`", please suggest something else.')
-return message.channel.send(nodareembed)
+return message.channel.send({embeds: [nodareembed]})
 }
 
 message.channel.send({
@@ -36,6 +36,18 @@ message.channel.send({
     {
     "type": 1,
     "components": [
+      {
+        "type": 2,
+        "label": "Agree",
+        "style": 3,
+        "custom_id": "agree"
+      },
+      {
+        "type": 2,
+        "label": "Deny",
+         "style": 4,
+         "custom_id": "deny"
+      },
       {
         type: 2,
         label: "Support Server", 
@@ -46,15 +58,13 @@ message.channel.send({
    
     }
     ],
+
   embeds: [new MessageEmbed().setColor('YELLOW').setAuthor(`Pending. . .`, message.author.avatarURL({dynamic: true})).setDescription(`You are about to send "\`${reportedbug}\`" to the support server as a suggestion. \n\nBy clicking **agree** you acknowledge ToD's **[Private Policy](https://github.com/NekWasTaken/ToD-Docs/blob/main/README.md#private-policy)** and any outcome of your suggestion (such as [getting blacklisted](https://github.com/NekWasTaken/ToD-Docs/blob/main/README.md#blacklist-faq) if your suggestion turns out to be a troll suggestion, or an abuse to the bot.) *Note: NSFW truths and dares are accepted*.\n\nIf you wish to cancel your suggestion, click **deny**.`).setFooter('This user interface will be deleted in 30 seconds if no choice is made.')]}).then(async m => {
     client.setTimeout(() => m.delete(), 30000);
 
-  embed: new MessageEmbed().setColor('RED').setAuthor(`Error.`, message.author.avatarURL({dynamic: true})).setDescription(`This command is currently being reworked and will be back and working soon. For now, you're welcome to join the support support serevr and send your suggestion in <#824333080596054026> .`)}).then(async m => {
-m.delete({timeout: 30000})
-/*
-bot.on('clickButton', async (button) => {
-if (button.id === 'agree') {
-await m.delete()
+    client.on('interactionCreate', async (interaction) => {
+if (interaction.customId === 'agree') {
+await client.setTimeout(() => m.delete(), 0);
 let bugreportembedbecausewhythefucknotsohereistheembedname = new MessageEmbed()
 .setTitle("<:image_20210516_090215:843518719506645003> New Suggestion! <:image_20210516_090215:843518719506645003>")
 .setThumbnail(message.author.avatarURL())
@@ -69,14 +79,14 @@ const webhookClient = new WebhookClient(
 );
 webhookClient.send({
   username: "ToD Suggestions",
-  avatarURL: bot.user.avatarURL(),
+  avatarURL: client.user.avatarURL(),
   embeds: [bugreportembedbecausewhythefucknotsohereistheembedname]
 });
 let finalembed = new MessageEmbed()
 .setTitle('Suggestion sent!')
 .setColor('GREEN')
 .setDescription(`Your suggestion "\`${reportedbug}\`" has successfully been sent to the development server to be reviewed! <:KannaPet:843534507419107338>`)
-return button.channel.send({
+return message.channel.send({
   "components": [
     {
     "type": 1,
@@ -94,13 +104,13 @@ return button.channel.send({
   embeds: [finalembed]})
   
 }
-if (button.id === 'deny') {
+if (interaction.customId === 'deny') {
   m.delete()
-  return button.channel.send({embed: new MessageEmbed().setColor('RED').setDescription('Successfully canceled your suggestion! <:Ranko_Pog:851883973403344947>')}).then(msg => client.setTimeout(() => message.delete(), 5000))
+  return message.channel.send({embeds: [new MessageEmbed().setColor('RED').setDescription('Successfully canceled your suggestion! <:Ranko_Pog:851883973403344947>')]}).then(msg => client.setTimeout(() => msg.delete(), 5000))
   
 }
 })
-*/
+
 
 })
 
