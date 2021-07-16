26fb4a20807df9811161c4566a481924e5773290
Zac <zezetta@icloud.com>
2021-07-15 20:50:51 -0700

    i love automation

---
+++ deploy.py
@@ -0,0 +1,11 @@
+import os
+import sys
+
+sharding = input("Start in sharded mode? (Y/n)")
+
+if sharding == "n":
+    print("Starting in non-sharded mode...")
+    os.system("node ToD.js")
+else:
+    print("Starting in sharded mode...")
+    os.system("node shards.js")
