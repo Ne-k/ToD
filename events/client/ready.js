ff0209104d59a6ad4a5115010779489a17e201a4
Nek <username.lilrice@gmail.com>
2021-07-29 09:45:25 -0700

    Auto posting fixes

--- events/client/ready.js
+++ events/client/ready.js
@@ -83,9 +83,8 @@ const Discord = require('discord.js')
 
 const promises = [
   bot.shard.fetchClientValues('guilds.cache.size'),
-  bot.shard.broadcastEval('this.guilds.cache.reduce((acc, guild) => acc + guild.memberCount, 0)')
+  bot.shard.broadcastEval(bot => bot.guilds.cache.size)
 ];
-
 return Promise.all(promises)
   .then(results => {
       const totalGuilds = results[0].reduce((acc, guildCount) => acc + guildCount, 0);
