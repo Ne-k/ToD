d7573142860ca199114fbbe64d173d4680aba174
Nek <username.lilrice@gmail.com>
2021-07-30 19:43:33 -0700

    Now uses a scraped version of mywaifulist.moe

--- commands/Anime corner/waifu.js
+++ commands/Anime corner/waifu.js
@@ -1,25 +1,24 @@
-const fetch = require('node-fetch')
-const waifuAPI = 'https://waifu.pics/api'
+
 const Discord = require('discord.js')
 
 module.exports = {
     config: {
         name: "waifu",
         usage: "waifu",
-        description: "Sends a embed with a random waifu picture from `https://waifu.pics`.",
+        description: "Sends a embed with a random waifu from `https://mywaifulist.moe`.",
     },
     run: async (bot, message, args) => {
 message.channel.startTyping()  
-        const waifu = new Discord.MessageEmbed()
-    
-        const { url } = await fetch(`${waifuAPI}/sfw/waifu`).then(res => res.json())
-    
-        waifu.setImage(url)
-        .setColor('RANDOM')
-          .setDescription(`Your requested waifu.`)
-         
-          message.channel.send({embeds: [waifu]})
+        require('node-fetch')(`https://japi.rest/mywaifulist/v1/random`, { headers: { 'Authorization': process.env.Japi_API_key}}).then(res => res.json()).then(d => {
+
+            
+            message.channel.send({embeds: [new Discord.MessageEmbed().setImage(d.data.image).setTitle(d.data.name).setURL(d.data.url).setDescription(d.data.description).setColor('GOLD')]})
     message.channel.stopTyping(true)
+    
+
+
+        })
+
 
 }
 }
