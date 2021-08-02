d5e011b37b4c13f9c932a8e254cfd84ef8d8a28a
Nek <username.lilrice@gmail.com>
2021-08-01 17:37:12 -0700

    Replaced buttons with dropdowns

--- commands/Anime corner/anisearch.js
+++ commands/Anime corner/anisearch.js
@@ -14,9 +14,7 @@ module.exports = {
   run: async (client, message, args) => {
 
 
-return message.channel.send({content: `The anime search command is currently disabled due to being broken, it'll be rewritten and added back in the future. `})
 
-/*
     if(!args.length) {
       return message.channel.send("Please give me a valid anime name on MAL.")
     }
@@ -39,77 +37,116 @@ return message.channel.send({content: `The anime search command is currently dis
        
           client.setTimeout(() => msg.delete(), 0)
        return message.channel.send({
-        components: [{
-          "type": 1, components: [
-              {
-                  "type": 2,
-          "label": '1️⃣', 
-          "style": 1, 
-          "custom_id": 'search1'
-              },
-              {
-                "type": 2,
-          "label": '2️⃣', 
-          "style": 1, 
-          "custom_id": 'Search2'
-              },
-              {
-                "type": 2,
-          "label": '3️⃣', 
-          "style": 1, 
-          "custom_id": 'Search3'
-                
-              },
-              {
-                "type": 2,
-          "label": '4️⃣', 
-          "style": 1, 
-          "custom_id": 'Search4'
-          
-              },
-              {
-                "type": 2,
-          "label": '5️⃣', 
-          "style": 1, 
-          "custom_id": 'Search5'
-          
-              },
-          ]},
-       {
-           type: 1, components: [
-            {
-              "type": 2,
-        "label": '6️⃣', 
-        "style": 1, 
-        "custom_id": 'Search6'
+        embeds: [new MessageEmbed().setColor('YELLOW').setTitle('Select the option corresponding to your query.').setDescription(`\`\`\`` + data.map((t, i) => `#${i + 1} - ${t.name}`).join('\n') + `\`\`\``).setFooter('Selection will be deleted in 5 seconds.')],
+        "components": [
+          {
+              "type": 1,
+              "components": [
+                  {
+                      "type": 3,
+                      "custom_id": 'Selects',
+                      "options":[
+                          
+                      
+                          
+                          {
+                              
+                              "label": `#1`,
+                              "value": "0",
+                              "description": `${data[0].name.substring(0, 45) + '. . .'}`,
+                          
+                          },
+                          // .substring(0, 4068) + ". . .")
+                          {
+                              
+                              "label": `#2`,
+                              "value": "1",
+                              "description": `${data[1].name.substring(0, 45) + '. . .'}`,
+                              
+                          
+                          },
+                          {
+                              "label": `#3`,
+                              "value": "2",
+                              "description": `${data[2].name.substring(0, 45) + '. . .'}`,
+                          
+                          },
+                          {
+                              "label": `#4`,
+                              "value": "3",
+                              "description": `${data[3].name.substring(0, 45) + '. . .'}`,
+                              
+                          },
+                          {
+                              "label": `#5`,
+                              "value": "4",
+                              "description": `${data[4].name.substring(0, 45) + '. . .'}`,
+                              
+                          },
+                          {
+                              "label": `#6`,
+                              "value": "5",
+                              "description": `${data[5].name.substring(0, 45) + '. . .'}`,
+                          },
+                          {
+                              "label": `#7`,
+                              "value": "6",
+                              "description": `${data[6].name.substring(0, 45) + '. . .'}`,
+                              
+                          },
+                          {
+                              "label": `#8`,
+                              "value": "7",
+                              "description": `${data[07].name.substring(0, 45) + '. . .'}`,
+                              
+                          },
+                          {
+                              "label": `#9`,
+                              "value": "8",
+                              "description": `${data[8].name.substring(0, 45) + '. . .'}`,
+                              
+                          },
+                          {
+                              "label": `#10`,
+                              "value": "9",
+                              "description": `${data[9].name.substring(0, 45) + '. . .'}`,
+                              
+                          },
+                        
+                          
+                          
+                         
+                      ],
+                      
+                      
+                      "placeholder": "Choose an anime",
+                      "default": true,
+                      "min_values": 1,
+                      "max_values": 1
+                  }
+                  
+              ]
+          }
+      ],
+
+
+
+      
         
-            },
-            {
-              "type": 2,
-        "label": 'Cancel', 
-        "style": 4, 
-        "custom_id": 'cancel'
-        
-            },
-           ]
-          }],
-         embeds: [new MessageEmbed().setColor('YELLOW').setTitle('Select the button corresponding to your query.').setDescription(`\`\`\`1. ${data[0].name}\n\n2. ${data[1].name}\n\n3. ${data[2].name}\n\n4. ${data[3].name}\n\n5. ${data[4].name}\n\n6. ${data[5].name}\`\`\``).setFooter('Selection will be deleted in 30 seconds.')]
         }).then(thing => {
 
-          client.setTimeout(() => thing.delete(), 30000)
+          client.setTimeout(() => msg.delete(), 300000)
 
 // Event Shit
 
             client.on('interactionCreate', async (interaction) => {
 
              if(interaction.guildId != message.guild.id) return;
-              if(interaction.user.id !== message.author.id) {
-                return interaction.reply({content: `You aren't allowed to click this button!`, ephemeral: true})
+              if(interaction.user.id !== message.author.id) return;
 
-              }
-                if(interaction.customId == 'search1') {
-                  client.setTimeout(() => thing.delete(), 0)
-                  message.channel.startTyping()
+                if(interaction.values[0] == 0) {
+                  
+                  
                   malScraper.getInfoFromName(data[0].name)
                   
      .then(async (s1) => { 
@@ -141,8 +178,8 @@ if(!s1.broadcast) {
              .addField('Characters:', "`" + s1.characters.map(ani => ani.name).join(` | `) + "`", true)
              .addField(`Trailer:`, `**[${s1.title}'s Trailer](${s1.trailer})**`, true)
 
-             message.channel.stopTyping(true)
-             return message.channel.send({embeds: [embed]})
+             
+             return thing.edit({embeds: [embed]})
 
      })
      
@@ -150,9 +187,9 @@ if(!s1.broadcast) {
               
               
               
-              if(interaction.customId == 'Search2') {
-                client.setTimeout(() => thing.delete(), 0)
-                message.channel.startTyping()
+              if(interaction.values[0] == 1) {
+                
+                
                   malScraper.getInfoFromName(data[1].name)
                   
      .then(async (s1) => { 
@@ -180,14 +217,14 @@ if(!s1.broadcast) {
              .addField('Anime Popularity:', `\`${s1.popularity}\``, true)
              .addField('Characters:', "`" + s1.characters.map(ani => ani.name).join(` | `) + "`", true)
              .addField(`Trailer:`, `**[${s1.title}'s Trailer](${s1.trailer})**`, true)
-             message.channel.stopTyping(true)
-              return message.channel.send({embeds: [embed]})
+             
+              return thing.edit({embeds: [embed]})
               
      })
               }
-              if(interaction.customId == 'Search3') {
-                client.setTimeout(() => thing.delete(), 0)
-                message.channel.startTyping()
+              if(interaction.values[0] == 2) {
+                
+                
                   malScraper.getInfoFromName(data[2].name)
                   
      .then(async (s1) => { 
@@ -215,14 +252,14 @@ if(!s1.broadcast) {
              .addField('Anime Popularity:', `\`${s1.popularity}\``, true)
              .addField('Characters:', "`" + s1.characters.map(ani => ani.name).join(` | `) + "`", true)
              .addField(`Trailer:`, `**[${s1.title}'s Trailer](${s1.trailer})**`, true)
-             message.channel.stopTyping(true)
-              return message.channel.send({embeds: [embed]})
+             
+              return thing.edit({embeds: [embed]})
              
      })
               }
-              if(interaction.customId == 'Search4') {
-                client.setTimeout(() => thing.delete(), 0)
-                message.channel.startTyping()
+              if(interaction.values[0] == 3) {
+                
+                
                   malScraper.getInfoFromName(data[3].name)
                   
      .then(async (s1) => { 
@@ -251,15 +288,15 @@ if(!s1.broadcast) {
              .addField('Anime Popularity:', `\`${s1.popularity}\``, true)
              .addField('Characters:', "`" + s1.characters.map(ani => ani.name).join(` | `) + "`", true)
              .addField(`Trailer:`, `**[${s1.title}'s Trailer](${s1.trailer})**`, true)
-             message.channel.stopTyping(true)
-              return message.channel.send({embeds: [embed]})
+             
+              return thing.edit({embeds: [embed]})
               
      })
               }
               
-              if(interaction.customId == 'Search5') {
-                client.setTimeout(() => thing.delete(), 0)
-                message.channel.startTyping()
+              if(interaction.values[0] == 4) {
+                
+                
                   malScraper.getInfoFromName(data[4].name)
                   
      .then(async (s1) => { 
@@ -286,14 +323,14 @@ if(!s1.broadcast) {
              .addField('Anime Popularity:', `\`${s1.popularity}\``, true)
              .addField('Characters:', "`" + s1.characters.map(ani => ani.name).join(` | `) + "`", true)
              .addField(`Trailer:`, `**[${s1.title}'s Trailer](${s1.trailer})**`, true)
-             message.channel.startTyping(true)
-              return message.channel.send({embeds: [embed]})
+             
+              return thing.edit({embeds: [embed]})
               
      })
               }
-              if(interaction.customId == 'Search6') {
-                client.setTimeout(() => thing.delete(), 0)
-                message.channel.startTyping()
+              if(interaction.values[0] == 5) {
+                
+                
                   malScraper.getInfoFromName(data[5].name)
                   
      .then(async (s1) => { 
@@ -321,9 +358,155 @@ if(!s1.broadcast) {
              .addField('Characters:', "`" + s1.characters.map(ani => ani.name).join(` | `) + "`", true)
              .addField(`Trailer:`, `**[${s1.title}'s Trailer](${s1.trailer})**`, true)
 
-             message.channel.stopTyping(true)
+             
 
-              return message.channel.send({embeds: [embed]})
+              return thing.edit({embeds: [embed]})
+              
+     })
+              }
+
+              if(interaction.values[0] == 6) {
+                
+                
+                  malScraper.getInfoFromName(data[6].name)
+                  
+     .then(async (s1) => { 
+      const { getColorFromURL } = require('color-thief-node');
+      if(s1.trailer === undefined) {
+        s1.trailer = `https://www.youtube.com/watch?v=dQw4w9WgXcQ`
+      }
+
+      const dominantColor = await getColorFromURL(s1.picture)
+              let embed = new MessageEmbed()
+              .setTitle(s1.title)
+              .setURL(s1.url)
+              .setColor(dominantColor)
+              .setDescription(s1.synopsis)
+              .setImage(s1.picture)
+              .addField('Alternative Titles:', `\`${s1.englishTitle}, ${s1.japaneseTitle}\``, true)
+              .addField('Total Episodes:', `\`${s1.episodes} episodes (${s1.duration})\``, true)
+             .addField('Aired:', `\`${s1.aired}\``, true)
+             .addField(`Rating:`, `\`${s1.rating}\``, true)
+             .addField(`Anime Status:`, `\`${s1.status}\``, true)
+             .addField('Genres:', `\`${s1.genres.join(`, `)}\``, true)
+             .addField('Score on MAL:', `\`${s1.score}/10 (${s1.scoreStats}) | \``, true)
+             .addField('Ranked:', `\`${s1.ranked}\``, true)
+             .addField('Anime Popularity:', `\`${s1.popularity}\``, true)
+             .addField('Characters:', "`" + s1.characters.map(ani => ani.name).join(` | `) + "`", true)
+             .addField(`Trailer:`, `**[${s1.title}'s Trailer](${s1.trailer})**`, true)
+
+             
+
+              return thing.edit({embeds: [embed]})
+              
+     })
+              }
+
+              if(interaction.values[0] == 7) {
+                
+                
+                  malScraper.getInfoFromName(data[7].name)
+                  
+     .then(async (s1) => { 
+      const { getColorFromURL } = require('color-thief-node');
+      if(s1.trailer === undefined) {
+        s1.trailer = `https://www.youtube.com/watch?v=dQw4w9WgXcQ`
+      }
+
+      const dominantColor = await getColorFromURL(s1.picture)
+              let embed = new MessageEmbed()
+              .setTitle(s1.title)
+              .setURL(s1.url)
+              .setColor(dominantColor)
+              .setDescription(s1.synopsis)
+              .setImage(s1.picture)
+              .addField('Alternative Titles:', `\`${s1.englishTitle}, ${s1.japaneseTitle}\``, true)
+              .addField('Total Episodes:', `\`${s1.episodes} episodes (${s1.duration})\``, true)
+             .addField('Aired:', `\`${s1.aired}\``, true)
+             .addField(`Rating:`, `\`${s1.rating}\``, true)
+             .addField(`Anime Status:`, `\`${s1.status}\``, true)
+             .addField('Genres:', `\`${s1.genres.join(`, `)}\``, true)
+             .addField('Score on MAL:', `\`${s1.score}/10 (${s1.scoreStats}) | \``, true)
+             .addField('Ranked:', `\`${s1.ranked}\``, true)
+             .addField('Anime Popularity:', `\`${s1.popularity}\``, true)
+             .addField('Characters:', "`" + s1.characters.map(ani => ani.name).join(` | `) + "`", true)
+             .addField(`Trailer:`, `**[${s1.title}'s Trailer](${s1.trailer})**`, true)
+
+             
+
+              return thing.edit({embeds: [embed]})
+              
+     })
+              }
+              if(interaction.values[0] == 8) {
+                
+                
+                  malScraper.getInfoFromName(data[8].name)
+                  
+     .then(async (s1) => { 
+      const { getColorFromURL } = require('color-thief-node');
+      if(s1.trailer === undefined) {
+        s1.trailer = `https://www.youtube.com/watch?v=dQw4w9WgXcQ`
+      }
+
+      const dominantColor = await getColorFromURL(s1.picture)
+              let embed = new MessageEmbed()
+              .setTitle(s1.title)
+              .setURL(s1.url)
+              .setColor(dominantColor)
+              .setDescription(s1.synopsis)
+              .setImage(s1.picture)
+              .addField('Alternative Titles:', `\`${s1.englishTitle}, ${s1.japaneseTitle}\``, true)
+              .addField('Total Episodes:', `\`${s1.episodes} episodes (${s1.duration})\``, true)
+             .addField('Aired:', `\`${s1.aired}\``, true)
+             .addField(`Rating:`, `\`${s1.rating}\``, true)
+             .addField(`Anime Status:`, `\`${s1.status}\``, true)
+             .addField('Genres:', `\`${s1.genres.join(`, `)}\``, true)
+             .addField('Score on MAL:', `\`${s1.score}/10 (${s1.scoreStats}) | \``, true)
+             .addField('Ranked:', `\`${s1.ranked}\``, true)
+             .addField('Anime Popularity:', `\`${s1.popularity}\``, true)
+             .addField('Characters:', "`" + s1.characters.map(ani => ani.name).join(` | `) + "`", true)
+             .addField(`Trailer:`, `**[${s1.title}'s Trailer](${s1.trailer})**`, true)
+
+             
+
+              return thing.edit({embeds: [embed]})
+              
+     })
+              }
+              if(interaction.values[0] == 9) {
+                
+                
+                  malScraper.getInfoFromName(data[9].name)
+                  
+     .then(async (s1) => { 
+      const { getColorFromURL } = require('color-thief-node');
+      if(s1.trailer === undefined) {
+        s1.trailer = `https://www.youtube.com/watch?v=dQw4w9WgXcQ`
+      }
+
+      const dominantColor = await getColorFromURL(s1.picture)
+              let embed = new MessageEmbed()
+              .setTitle(s1.title)
+              .setURL(s1.url)
+              .setColor(dominantColor)
+              .setDescription(s1.synopsis)
+              .setImage(s1.picture)
+              .addField('Alternative Titles:', `\`${s1.englishTitle}, ${s1.japaneseTitle}\``, true)
+              .addField('Total Episodes:', `\`${s1.episodes} episodes (${s1.duration})\``, true)
+             .addField('Aired:', `\`${s1.aired}\``, true)
+             .addField(`Rating:`, `\`${s1.rating}\``, true)
+             .addField(`Anime Status:`, `\`${s1.status}\``, true)
+             .addField('Genres:', `\`${s1.genres.join(`, `)}\``, true)
+             .addField('Score on MAL:', `\`${s1.score}/10 (${s1.scoreStats}) | \``, true)
+             .addField('Ranked:', `\`${s1.ranked}\``, true)
+             .addField('Anime Popularity:', `\`${s1.popularity}\``, true)
+             .addField('Characters:', "`" + s1.characters.map(ani => ani.name).join(` | `) + "`", true)
+             .addField(`Trailer:`, `**[${s1.title}'s Trailer](${s1.trailer})**`, true)
+
+             
+
+              return thing.edit({embeds: [embed]})
               
      })
               }
@@ -346,7 +529,7 @@ if(!s1.broadcast) {
         
       } catch (err) {
         msg.delete();
-        console.log(chalk.redBright(`[MAL COMMAND ERROR]`) + ` - ${err}`)
+        console.log(err)
          return message.channel.send({content: `Unable to find \`${args.join(" ")}\` on MyAnimeList, this is most likely an error.`});
          
        }                      
@@ -359,7 +542,7 @@ if(!s1.broadcast) {
 
      
   
-     */
+     
     
        
                        
