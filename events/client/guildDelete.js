f744230f2d5b78e01c9d2f4520f6068f7786389b
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-10-21 21:49:36 -0700

    Fixed guildDelete

--- events/client/guildDelete.js
+++ events/client/guildDelete.js
@@ -3,12 +3,12 @@ module.exports = async (client, guild) => {
     client.db.delete(`prefix_${guild.id}`);
     client.db.delete(`nsfwEnabled_${guild.id}`);
     client.db.delete(`antiscamEnabled_${guild.id}`)
-    client.db.delete(`mutedRole_${message.guild.id}`)
+    client.db.delete(`mutedRole_${guild.id}`)
 
     require("colors");
     console.log(
       `
-    Left a guild, data wipe has been successful.`.green
+    Left guild ${guild.id}, data wipe has been successful.`.green
     );
   } catch (e) {
     console.log(
