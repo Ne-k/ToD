5357950d5c2549360f589734e6613848e7bcddc8
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-10-28 11:24:26 -0700

    Yeah, I fucked it.

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -13,7 +13,7 @@ module.exports = async (bot, message) => {
 
   if(message.content && db.fetch(`antiscamEnabled_${message.guild.id}`) == true) {
 
-    if(/^([0-9A-Za-z-\.@:%_\+~#=]+)+((\.[a-zA-Z]{1,2})+)(\/(.)*)?(\?(.)*)?/gi.test(message.content) || /^(http[s]?:\/\/(www\.)?|ftp:\/\/(www\.)?|www\.){1}([0-9A-Za-z-\.@:%_\+~#=]+)+((\.[a-zA-Z]{2,3})+)(\/(.)*)?(\?(.)*)?/gi.test(message.content)) {
+   
 
       try {
         const unix = Math.floor(new Date().getTime() / 1000);
@@ -58,7 +58,6 @@ module.exports = async (bot, message) => {
         console.log(e)
       }
     
-    }
       
   }
 
