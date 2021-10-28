7f940ef7fb237bbb9e9f33b4337a3e036142399e
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-10-27 17:30:01 -0700

    Oops

--- events/guild/messageCreate.js
+++ events/guild/messageCreate.js
@@ -15,7 +15,6 @@ module.exports = async (bot, message) => {
 
     if(/^(http[s]?:\/\/(www\.)?|ftp:\/\/(www\.)?|www\.){1}([0-9A-Za-z-\.@:%_\+~#=]+)+((\.[a-zA-Z]{2,3})+)(\/(.)*)?(\?(.)*)?/gi.test(message.content)) {
 
-      console.log(message.content)
       try {
         const unix = Math.floor(new Date().getTime() / 1000);
 
