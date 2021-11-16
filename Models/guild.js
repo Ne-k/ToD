93666086a4cc84cd23a02a73c167e10bf95ceec5
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-11-15 18:45:21 -0800

    Removed useless stuff

--- Models/guild.js
+++
@@ -1,8 +0,0 @@
-const {Schema, model} = require("mongoose");
-module.exports = model(
-    "Guild",
-    new Schema({
-        guildID: {type: String, default: null},
-        allowNSFW: {type: Boolean, default: false},
-    })
-);
