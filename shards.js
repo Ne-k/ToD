310e9251a25ee79db4128f29bf553a23f05f4e25
Nguyen <68248147+Ne-k@users.noreply.github.com>
2021-12-27 16:34:22 -0800

    Removed unused stuff

--- shards.js
+++ shards.js
@@ -1,21 +1,10 @@
 const {ShardingManager} = require("discord.js");
-const EventEmitter = require("events");
-const Statcord = require("statcord.js");
 require("dotenv").config();
 
 const manager = new ShardingManager("./ToD.js", {
     token: process.env.token,
     autoSpawn: true,
     respawn: true,
-});
-
-const statcord = new Statcord.ShardingClient({
-    key: "statcord.com-iFfsaDeC8KtKtUrlxgHk",
-    manager,
-    postCpuStatistics: true /* Whether to post CPU statistics or not, defaults to true */,
-    postMemStatistics: true /* Whether to post memory statistics or not, defaults to true */,
-    postNetworkStatistics: true /* Whether to post memory statistics or not, defaults to true */,
-    autopost: true /* Whether to auto post or not, defaults to true */,
 });
 
 require("colors");
