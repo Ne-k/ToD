996815bb285d5343a1216dc490fe0b614763e0fd
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-12-08 14:08:20 -0800

    Status

--- events/ready.js
+++ events/ready.js
@@ -3,7 +3,7 @@ const client = require('../index');
 require("@colors/colors")
 const schema = require("../Database/guildConfigSchema")
 client.on("ready", () => {
-	client.user.setActivity("Truth or Dare", { type: ActivityType.Playing });
+	client.user.setActivity("Currently Under maintenance. . .", { type: ActivityType.Watching });
 
 	client.shard.broadcastEval((bot) => bot.guilds.cache.size).then((res) => {
 		console.log(`[ Client ]`.blue + ` Logged in as ${client.user.tag} | loaded ${res.reduce((prev, val) => prev + val, 0).toLocaleString()} servers and ${client.options.shardCount.toLocaleString()} shards.`.magenta);
