1ad2a9b69ce5720cedb1b2b665d0e08b4691a0ac
Nguyen <68248147+Ne-k@users.noreply.github.com>
2022-12-19 20:59:51 -0800

    asd

--- events/guildDelete.js
+++ events/guildDelete.js
@@ -19,6 +19,7 @@ client.on("guildDelete", async (guild) => {
     })
     Schema.findOneAndDelete({ id: guild.id }, (err, res) => {
         if (err) console.log(err)
+        if (res) console.log(`[ Database ] `.magenta + `Deleted guild ${guild.name} (${guild.id}) from database`)
     })
 
 })
