a0b1ab0bf1ba975f1f7c33a24e7c0b83714efa94
Nguyen <68248147+Ne-k@users.noreply.github.com>
2023-05-02 20:19:50 -0700

    asdas

--- slashCommands/fun/dare.js
+++ slashCommands/fun/dare.js
@@ -1,6 +1,7 @@
 const { ApplicationCommandType, ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require('discord.js');
 const Discord = require("discord.js");
 const Schema = require("../../Database/guildConfigSchema");
+const fetch = require("cross-fetch");
 
 module.exports = {
     name: 'dare',
@@ -11,17 +12,35 @@ module.exports = {
     run: async (client, interaction) => {
 
         Schema.findOne({guildID: interaction.guild.id}, async (err, data) => {
+            const apiDare = await fetch("https://api.truthordarebot.xyz/v1/truth").then(res => res.json());
+            let sfwDare;
+            let nsfwDare;
+            if (Math.random() < 0.5) {
+                sfwDare = client.tod.Dare[Math.floor(Math.random() * client.tod.Dare.length)];
+            }
+            else {
+                sfwDare = apiDare.question;
+            }
+
+            if(Math.random() < 0.33) {
+                nsfwDare = client.tod.nsfwDare[Math.floor(Math.random() * client.tod.nsfwDare.length)];
+            }
+            else if(Math.random() < 0.66) {
+                nsfwDare = apiDare.question;
+            }
+            else {
+                nsfwDare = client.tod.nsfwDare[Math.floor(Math.random() * client.tod.nsfwDare.length)];
+            }
             if(!data) {
                 await new Schema({
                     guildID: interaction.guild.id,
                     guildName: interaction.guild.name,
                 }).save();
             } else if(data.config.nsfwToggle === false) {
-                const dare = client.tod.Dare[Math.floor(Math.random() * client.tod.Dare.length)];
                 const embed =  new EmbedBuilder()
                     .setColor("Random")
                     .setTitle("Dare")
-                    .setDescription(dare)
+                    .setDescription(sfwDare)
                 if(Math.floor(Math.random() * 100) === 1) {
                     embed.setFooter({text: "Want to suggest more dare questions? Feel free and use the /suggest command!"})
                 }
@@ -31,11 +50,10 @@ module.exports = {
                     ]
                 })
             } else {
-                const dare = client.tod.Dare.concat(client.tod.nsfwDare)[Math.floor(Math.random() * client.tod.Dare.concat(client.tod.nsfwDare).length)];
                 const embed =  new EmbedBuilder()
                     .setColor("Random")
                     .setTitle("Dare")
-                    .setDescription(dare)
+                    .setDescription(nsfwDare)
                 if(Math.floor(Math.random() * 100) === 1) {
                     embed.setFooter({text: "Want to suggest more dare questions? Feel free and use the /suggest command!"})
                 }
--- slashCommands/fun/truth.js
+++ slashCommands/fun/truth.js
@@ -1,6 +1,7 @@
 const { ApplicationCommandType, ActionRowBuilder, ButtonBuilder, EmbedBuilder } = require('discord.js');
 const Discord = require("discord.js");
 const Schema = require("../../Database/guildConfigSchema");
+const fetch = require("cross-fetch");
 module.exports = {
     name: 'truth',
     description: "get a random truth question",
@@ -9,20 +10,34 @@ module.exports = {
     cooldown: 3000,
     run: async (client, interaction) => {
         Schema.findOne({guildID: interaction.guild.id}, async (err, data) => {
+            const apiTruths = await fetch("https://api.truthordarebot.xyz/v1/truth").then(res => res.json());
+            let sfwTruths;
+            let nsfwTruths;
+            if (Math.random() < 0.5) {
+                sfwTruths = client.tod.Truth[Math.floor(Math.random() * client.tod.Truth.length)];
+            } else {
+                sfwTruths = apiTruths.question;
+            }
 
 
+            if (Math.random() < 0.33) {
+                nsfwTruths = client.tod.Truth[Math.floor(Math.random() * client.tod.Truth.length)];
+            } else if (Math.random() < 0.66) {
+                nsfwTruths = apiTruths.question;
+            } else {
+                nsfwTruths = client.tod.nsfwTruth[Math.floor(Math.random() * client.tod.nsfwTruth.length)];
+            }
             if(!data) {
                 await new Schema({
                     guildID: interaction.guild.id,
                     guildName: interaction.guild.name,
                 }).save();
 
-            } else if(data.config.nsfwToggle === false) {
-                const truth = client.tod.Truth[Math.floor(Math.random() * client.tod.Truth.length)];
+            } else if(!data.config.nsfwToggle) {
                 const embed = new EmbedBuilder()
                     .setColor("Random")
                     .setTitle("Truth")
-                    .setDescription(truth)
+                    .setDescription(sfwTruths)
                 if(Math.floor(Math.random() * 100) === 1) {
                     embed.setFooter({text: "Want to suggest more truth questions? Feel free and use the /suggest command!"})
                 }
@@ -33,12 +48,10 @@ module.exports = {
                     ]
                 })
             } else {
-                const truth = client.tod.Truth.concat(client.tod.nsfwTruth)[Math.floor(Math.random() * client.tod.Truth.concat(client.tod.nsfwTruth).length)];
-
                 const embed = new EmbedBuilder()
                     .setColor("Random")
                     .setTitle("Truth")
-                    .setDescription(truth)
+                    .setDescription(nsfwTruths)
                 if(Math.floor(Math.random() * 100) === 1) {
                     embed.setFooter({text: "Want to suggest more truth questions? Feel free and use the /suggest command!"})
                 }
